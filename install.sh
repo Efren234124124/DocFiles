5ea57f8051108a5823ef920f70283f8bd4634c7f
Efren234124124 <l22020380@morelia.tecnm.mx>
2024-11-28 11:52:51 -0600

    Actualizaciòn
    Se agrego la instalaciòn del php y una confirmaciòn de la verciòn

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#Instalaciòn de una imagen en la carpeta descargas
 wget -O ~/Imágenes/Descargas/supra.jpg  "https://encrypted-tbn0.gstatic.com/i>
 cd ~/Imágenes/Descargas
 ls
 echo "Se movio Correctamente a Imagenes"
+
+#Instalacion del php
+sudo apt install php -y
+php -v
+echo "Se instalo correctamente el php"
